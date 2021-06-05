29741928b0e1c813c2389f9770a3db5cac954fb7
velez <brahianvelezsv@gmail.com>
2021-06-04 19:58:39 -0500

    Se añadieron comentarios

--- Js/app.js
+++ Js/app.js
@@ -1,18 +1,24 @@
 "use strict";
-
+// Creamos el metodo 'load' para esperar quer todo el html cargue y el script pueda estar en el head.
 window.addEventListener("load", () => {
+  // Llamamos las imagenes con el querySelector para mostrarlas al no ingresar nada
   var imagen1 = document.querySelector("#imagen1");
   var imagen2 = document.querySelector("#imagen2");
-
+  // creamos la sintaxis basica de jquery para llamar el metodo ajax
   $(document).ready(function () {
+    // creamos la funcion para cuando demos click en el boton de guardar y confirmamos si los 2 campos tienen caracteres
+    // en los input
     $("#btnguardar").click(function () {
       if ($("#nombre").val() != "") {
         if ($("#apellido").val() != "") {
           var datos_usuario = $("#formPrincipal").serialize();
           $.ajax({
+            //Creamos la sintaxis basica del ajax y en la url seleccionamos el archivo php
             type: "POST",
             url: "php/insertar.php",
             data: datos_usuario,
+            // al completar correctamente el metodo de php aparecera la alerta de guardado exitosamente de lo contrario
+            // aparecera la de intentar nuevamente por error en el servidor
             success: function (respuesta) {
               if (respuesta == 1) {
                 alert("Datos guardados exitosamente");
@@ -23,6 +29,8 @@ window.addEventListener("load", () => {
               }
             },
           });
+          // empleamos la alerta de que faltan campos por llenar y mostramos una imagen en el campo que hace falta para 
+          // que el usuario localice el error facilmente
         } else {
           alert("Por favor ingresar su apellido en donde aparece la letra X");
           imagen2.style.display = "inline-block";
--- php/insertar.php
+++ php/insertar.php
@@ -1,9 +1,10 @@
 <?php
+    // Creamos la conexion con el servidor con sus respectivos parametros
     $conexion=mysqli_connect('localhost','root','','prueba');
-
+    //Mandamos a llamar los campos que almacenaron los datos
     $nombre=$_POST['nombre'];
     $apellido=$_POST['apellido'];
-
+    //Utilizamos el metodo insert para agregar los datos a la base de datos mysql
     $sql = "Insert into usuarios (nombre,apellido) values ('$nombre','$apellido')";
     
         echo mysqli_query($conexion,$sql);
